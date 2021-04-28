b9193b5800b860d185d0dd8abb76ebf08aba3125
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-27 21:28:30 -0400

    navbar rendering successfully

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Employee Directory</span>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav;
