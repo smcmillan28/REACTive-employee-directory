10ab5d5cc3844ad7985f22253762f449e5ab870e
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-27 22:55:12 -0400

    list of employees populating in table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from "react";
-import EmployeeContainer from "./components/EmployeeContainer";
+import React, { Component } from "react";
+// import EmployeeContainer from "./components/EmployeeContainer";
+import Nav from "./components/Nav.js";
+import Jumbo from "./components/Jumbo.js";
+import employees from "./employees.json";
 
-function App() {
-  return <EmployeeContainer />;
+// function App() {
+//   return <EmployeeContainer />;
+// }
+
+class App extends Component {
+  state = {
+    employees
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Jumbo />
+        <div>
+          <table className="table table-dark table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Employee Age</th>
+                <th scope="col">Image</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            {this.state.employees.map(employee => (
+              <tbody>
+                <tr>
+                  <th scope="row">{employee.dob.age}</th>
+                  <td><img src={employee.picture.thumbnail}></img></td>
+                  <td>{employee.name.first}</td>
+                  <td>{employee.name.last}</td>
+                  <td>{employee.email}</td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -5,11 +5,11 @@ function Jumbo() {
         <div>
             <div className="jumbotron">
                 <h3 className="display-4">Welcome to your employee directory!</h3>
-                <p className="lead">Below you will find basic information about the hard workers that make your company go.  You can sort by first and last name, and filter by last name.</p>
+                <p className="lead">Below you will find basic information about the hard workers that make your company go.  You can sort by age, and filter by last name.</p>
                 <hr className="my-4"></hr>
             </div>
         </div>
     )
 }
 
-export default Jumbo;
+export default Jumbo; 
