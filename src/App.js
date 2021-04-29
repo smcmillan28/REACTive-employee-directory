e95018b6cbad8b0483f253fc0e471595dc500dc1
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-28 21:37:15 -0400

    user can sort age up and down

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
 import Nav from "./components/Nav.js";
 import Jumbo from "./components/Jumbo.js";
-// import Table from "./components/Table.js";
 import employees from "./employees.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortAmountDown } from "@fortawesome/free-solid-svg-icons";
+import { faSortAmountDown, faSortAmountUp } from "@fortawesome/free-solid-svg-icons";
 
 const sortDown = <FontAwesomeIcon icon={faSortAmountDown} />
+const sortUp = <FontAwesomeIcon icon={faSortAmountUp} />
 
 class App extends Component {
   state = {
     employees
   };
 
-  sortAge = () => {
+  sortAgeDown = () => {
     const employeesDown = this.state.employees.sort((a, b) => (a.dob.age > b.dob.age) ? 1 : -1);
-    const employeesUp = this.state.employees.sort((a, b) => (b.dob.age > a.dob.age) ? 1 : -1);
+    this.setState({ employeesDown });
+  }
 
-    if (this.state === employeesUp) {
-      this.setState({ employeesDown });
-    } else if (this.state === employeesDown) {
-      this.setState({ employeesUp });
-    }
+  sortAgeUp = () => {
+    const employeesUp = this.state.employees.sort((a, b) => (b.dob.age > a.dob.age) ? 1 : -1);
+    this.setState({ employeesUp });
   }
 
   render() {
@@ -34,7 +33,7 @@ class App extends Component {
             <thead>
               <tr>
                 <th scope="col">Employee Username</th>
-                <th scope="col">Employee Age <button onClick={this.sortAge}>{sortDown}</button></th>
+                <th scope="col">Employee Age <button onClick={this.sortAgeDown}>{sortDown}</button> <button onClick={this.sortAgeUp}>{sortUp}</button></th>
                 <th scope="col">Image</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
@@ -44,7 +43,7 @@ class App extends Component {
             {this.state.employees.map(employee => (
               <tbody>
                 <tr>
-                  <th>{employee.login.username}</th>
+                  <td>{employee.login.username}</td>
                   <th scope="row">{employee.dob.age}</th>
                   <td><img src={employee.picture.thumbnail} alt="employee"></img></td>
                   <td>{employee.name.first}</td>
