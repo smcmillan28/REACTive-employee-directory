a7d8a012f1163da1f39f82f51dfa45c2f57e09c5
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-28 21:23:56 -0400

    can sort by age in one direction, need to have the sort work both ways

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
-// import EmployeeContainer from "./components/EmployeeContainer";
 import Nav from "./components/Nav.js";
 import Jumbo from "./components/Jumbo.js";
+// import Table from "./components/Table.js";
 import employees from "./employees.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortAmountDown } from "@fortawesome/free-solid-svg-icons";
 
-// function App() {
-//   return <EmployeeContainer />;
-// }
+const sortDown = <FontAwesomeIcon icon={faSortAmountDown} />
 
 class App extends Component {
   state = {
     employees
   };
+
+  sortAge = () => {
+    const employeesDown = this.state.employees.sort((a, b) => (a.dob.age > b.dob.age) ? 1 : -1);
+    const employeesUp = this.state.employees.sort((a, b) => (b.dob.age > a.dob.age) ? 1 : -1);
+
+    if (this.state === employeesUp) {
+      this.setState({ employeesDown });
+    } else if (this.state === employeesDown) {
+      this.setState({ employeesUp });
+    }
+  }
 
   render() {
     return (
@@ -22,7 +33,8 @@ class App extends Component {
           <table className="table table-dark table-hover">
             <thead>
               <tr>
-                <th scope="col">Employee Age</th>
+                <th scope="col">Employee Username</th>
+                <th scope="col">Employee Age <button onClick={this.sortAge}>{sortDown}</button></th>
                 <th scope="col">Image</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
@@ -32,6 +44,7 @@ class App extends Component {
             {this.state.employees.map(employee => (
               <tbody>
                 <tr>
+                  <th>{employee.login.username}</th>
                   <th scope="row">{employee.dob.age}</th>
                   <td><img src={employee.picture.thumbnail} alt="employee"></img></td>
                   <td>{employee.name.first}</td>
