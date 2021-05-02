cffcdb1a7251691d5c344ad13a1d5cd09584440b
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-02 00:00:13 -0400

    adjusted package.json for deployment

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ function Nav(props) {
                 <p className="navbar-brand">GoGo Gadgets - Employee Directory</p>
                 <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Search By Last Name" />
-                        <button onClick={() => props.filterName()} className="btn btn-outline-primary">Search</button>
+                        <button onClick={() => props.filterName} className="btn btn-outline-primary">Search</button>
                 </form>
             </div>
         </nav>
